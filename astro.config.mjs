501643f513272300f295d1c551b45f18a95b9e4f
赵睿 <zhaorui@baiwang.com>
2023-11-03 14:19:29 +0800

    feat: 禁用浏览器下拉刷新

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
 import { defineConfig } from 'astro/config';
-import vue from "@astrojs/vue";
-import { VitePWA } from 'vite-plugin-pwa'
+import vue from '@astrojs/vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-  vite:{
-    plugins:[VitePWA({
-      manifest:{
-        icons: [
-          {
-            src: 'main.png',
-            sizes: '610x610',
-            type: 'image/png'
-          }
-        ]}
-    })]
-  }
+  vite: {
+    plugins: [
+      VitePWA({
+        devOptions: {
+          enabled: true,
+        },
+        manifest: {
+          icons: [
+            {
+              src: 'main.png',
+              sizes: '610x610',
+              type: 'image/png',
+            },
+          ],
+        },
+      }),
+    ],
+  },
 });
