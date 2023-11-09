24684353240bdaa78665387cd47ff4a5b43e739e
赵睿 <zhaorui@baiwang.com>
2023-11-09 16:01:38 +0800

    feat: 使用tab, 加入echarts

---
+++ src/view/Progress/Main.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+import { Tab, Tabs } from 'vant';
+import List from './List.vue';
+import Edit from './Edit.vue';
+import Statistics from './Statistics.vue';
+
+const active = ref();
+</script>
+<template>
+  <Tabs v-model:active="active" class="h-full" sticky>
+    <Tab title="列表" >
+      <div class="grid h-full" style="grid-template-rows: 1fr min-content">
+        <List />
+        <Edit />
+      </div>
+    </Tab>
+    <Tab title="统计"><Statistics></Statistics></Tab>
+  </Tabs>
+</template>
+<style></style>
