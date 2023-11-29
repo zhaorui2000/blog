8b36ec658f7036cc3e0985193aad726305dc3742
Zhao Rui <wiserui1996@hotmail.com>
2023-11-29 21:52:02 +0800

    feat: 更新 .van-tabs.h-full 样式

--- src/view/Progress/Main.vue
+++ src/view/Progress/Main.vue
@@ -1,21 +1,23 @@
 <script setup>
 import { ref } from 'vue';
 import { Tab, Tabs } from 'vant';
+import Statistics from './Statistics.vue';
 import List from './List.vue';
 import Edit from './Edit.vue';
-import Statistics from './Statistics.vue';
 
 const active = ref();
 </script>
 <template>
-  <Tabs v-model:active="active" class="h-full" sticky>
-    <Tab title="列表" >
-      <div class="grid h-full" style="grid-template-rows: 1fr min-content">
+  <Tabs v-model:active="active" class="h-full">
+    <Tab title="列表">
+      <div id="abcdefgh" class="grid h-full" style="grid-template-rows: 1fr min-content">
         <List />
         <Edit />
       </div>
     </Tab>
-    <Tab title="统计"><Statistics></Statistics></Tab>
+    <Tab title="统计">
+      <Statistics></Statistics>
+    </Tab>
   </Tabs>
 </template>
 <style></style>
