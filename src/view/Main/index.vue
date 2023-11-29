b79921532aa58dd8cf03cf7fc31a820d451193f7
Zhao Rui <wiserui1996@hotmail.com>
2023-11-29 22:57:13 +0800

    feat: 加入NavBar 和首页导航

---
+++ src/view/Main/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { Cell, CellGroup } from 'vant';
+</script>
+
+<template>
+  <CellGroup>
+    <Cell is-link title="进度" url="/progress"></Cell>
+  </CellGroup>
+</template>
+
+<style scoped></style>
---
+++ src/view/NavBar/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { NavBar } from 'vant';
+
+function handleClickLeft() {
+  location.href = `${location.origin}${location.pathname.split('/').slice(0, -1).join('/')}`;
+}
+</script>
+
+<template>
+  <NavBar left-arrow left-text="返回" @click-left="handleClickLeft"></NavBar>
+</template>
+
+<style scoped></style>
