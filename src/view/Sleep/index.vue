8c15520db28deb404bc8fa39d2f08fa5167236a4
Zhao Rui <wiserui1996@hotmail.com>
2023-12-05 23:21:11 +0800

    feat: 睡眠视频时长临时设置为10分钟后停止

--- src/view/Sleep/index.vue
+++ src/view/Sleep/index.vue
@@ -1,15 +1,30 @@
 <script setup>
 import { onMounted, ref } from 'vue';
+import { interval, take } from 'rxjs';
 
 const player = ref();
 
 onMounted(async () => {
   const { default: Player } = await import('xgplayer');
-  new Player({
+  const p = new Player({
     el: player.value,
     url: '/478.mp4',
+    width: 648,
+    height: 1080,
     fluid: true,
+    fitVideoSize: 'fixHeight',
+    loop: true,
+    pip: true,
   });
+
+  interval(1000)
+    .pipe(take(601))
+    .subscribe((x) => {
+      console.log(x);
+      if (x === 600) {
+        p.destroy();
+      }
+    });
 });
 </script>
 
