c15df55e1ed67cf9c98e4ac9486fc1adc9eada51
Zhao Rui <wiserui1996@hotmail.com>
2023-12-18 23:08:08 +0800

    faet: 删掉统计tab

--- src/view/Progress/Main.vue
+++ src/view/Progress/Main.vue
@@ -1,7 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import { Tab, Tabs } from 'vant';
-import Statistics from './Statistics.vue';
+import { onMounted } from 'vue';
 import List from './List.vue';
 import Edit from './Edit.vue';
 import { $groupIndex, $progressGroup, $progressList } from '@store/progressStore';
@@ -9,25 +7,15 @@ import { useStore } from '@nanostores/vue';
 
 const groupIndex = useStore($groupIndex);
 const progressGroup = useStore($progressGroup);
-const progressList = useStore($progressList);
 
 onMounted(() => {
   $progressList.set($progressGroup.get()?.[groupIndex.value]?.value ?? []);
 });
-
-const active = ref();
 </script>
 <template>
-  <Tabs v-model:active="active" class="h-full">
-    <Tab title="列表">
-      <div class="grid h-full" style="grid-template-rows: 1fr min-content">
-        <List />
-        <Edit />
-      </div>
-    </Tab>
-    <Tab title="统计">
-      <Statistics></Statistics>
-    </Tab>
-  </Tabs>
+  <div class="grid h-full" style="grid-template-rows: 1fr min-content">
+    <List />
+    <Edit />
+  </div>
 </template>
 <style></style>
