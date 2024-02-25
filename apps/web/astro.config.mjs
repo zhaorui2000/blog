4ebd45831013efd51c900d428c72e325aebfdb93
Zhao Rui <wiserui1996@hotmail.com>
2024-02-25 22:37:17 +0800

    feat: 配置 netlify

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   // ...
   integrations: [tailwind()],
+  output: 'server',
+  adapter: netlify({
+    cacheOnDemandPages: true,
+  }),
 });
