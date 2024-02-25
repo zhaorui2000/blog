b302dca2dd371a5a5e731432660940fc1a359b3f
Zhao Rui <wiserui1996@hotmail.com>
2024-02-25 23:50:39 +0800

    feat: 加入 Spotlight

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
 
 export default defineConfig({
   // ...
-  integrations: [tailwind()],
+  integrations: [tailwind(), sentry(), spotlightjs()],
   output: 'server',
   adapter: netlify({
     cacheOnDemandPages: true,
