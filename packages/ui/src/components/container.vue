600229ecc6f0cdbe3fd54dc6511324ada853816b
赵睿 <zhaorui@baiwang.com>
2024-02-27 17:27:44 +0800

    feat: 更新 ui 库

--- packages/ui/src/components/container.vue
+++ packages/ui/src/components/container.vue
@@ -1,5 +1,9 @@
 <script setup></script>
 <template>
-  <div class="bg-N1 grid"><slot></slot></div>
+  <div class="bg-N1 grid w-screen max-h-[100vh] h-full overflow-hidden">
+    <slot>
+      <van-empty />
+    </slot>
+  </div>
 </template>
 <style></style>
