7618abb46bf81a4e7566364f8a995f778a362080
Zhao Rui <wiserui1996@hotmail.com>
2024-04-16 20:39:15 +0800

    feat: 优化滚动样式

--- apps/web/src/pages/_components/List.vue
+++ apps/web/src/pages/_components/List.vue
@@ -5,10 +5,13 @@ import { useStore } from '@nanostores/vue';
 import { CellGroup, Cell, TimeTag } from '@blog/ui';
 import LabelList from './LabelList.vue';
 import { isNil } from '@blog/utils';
-import { computed } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 const list = useStore($list);
 const currentSelectLabel = useStore($currentSelectLabel);
 const endOfDay = useStore($endOfDay);
+const stickyElement = ref();
+const itemElement = ref();
+const titleElement = ref();
 const computedList = computed(() => {
   return list.value.filter((item) => {
     if (isNil(item.label) || item.label === '') {
@@ -17,10 +20,15 @@ const computedList = computed(() => {
     return item.label === currentSelectLabel.value;
   });
 });
+onMounted(() => {
+  const { height: stickyHeight } = window.getComputedStyle(stickyElement.value);
+  const { height: titleHeight } = window.getComputedStyle(stickyElement.value);
+  itemElement.value.style.height = `calc(100% + ${stickyHeight} + ${titleHeight})`;
+});
 </script>
 <template>
   <CellGroup>
-    <Cell title="结束时间">
+    <Cell title="结束时间" ref="titleElement">
       <template #title>
         <div class="flex items-center gap-2">
           <div>一天结束时间</div>
@@ -28,8 +36,12 @@ const computedList = computed(() => {
         </div>
       </template>
     </Cell>
-    <LabelList></LabelList>
-    <Item v-for="{ id, index } of computedList" :index="index" :key="id"></Item>
+    <div class="sticky top-0 z-10" ref="stickyElement">
+      <LabelList></LabelList>
+    </div>
+    <div ref="itemElement">
+      <Item v-for="{ id, index } of computedList" :index="index" :key="id"></Item>
+    </div>
   </CellGroup>
 </template>
 <style></style>
